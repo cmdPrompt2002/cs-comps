4774ac378b2d9cc3bfbbffe5408d106deb914dfb
Prompt Eua-anant <109594102+cmdPrompt2002@users.noreply.github.com>
2024-10-13 17:22:36 -0500

    status update and fixed options parsing

--- thing/thing.c
+++ thing/thing.c
@@ -4,18 +4,24 @@
 #include <unistd.h>
 #include <libssh/libssh.h>
 
-//Candidate names: passwordSquirt, sprinker, octopus, 
+int sshAttempt(char* destination, char* username, char* password, ssh_session myssh);
+
+//Global variables
+char *service;
+char *destination;
+int port;
+char *usr;
+char *usrFilename;
+char *pass;
+char *passFilename;
+FILE *usrFile;
+FILE *passFile;
+
+//Candidate names: passwordSquirt, sprinkler, octopus, passqrt, squirt, passquirt, 
 /*Command line parsing*/
 int main(int argc, char *argv[]) {
     int c;
-    char *service;
-    int port;
-    char *usr;
-    char *usrFilename;
-    char *pass;
-    char *passFilename;
-    FILE *usrFile;
-    FILE *passFile;
+    
 
     /*Expected input:
         name service destination [-s PORT] [-u USERNAMES] [-p PASSWORD] {more stuff later}
@@ -24,12 +30,10 @@ int main(int argc, char *argv[]) {
     
 
     /*Command line parsing*/
-
-    //Service and destination
-
-    //Options parsing
-    while ((c = getopt(argc, argv, ":s:u:U:p:P:")) != -1) {
-        printf("%d" , c);
+    //printf("optind: %i, arg: %s\n", optind, argv[optind]);
+    while (optind < argc) {
+        c = getopt(argc, argv, ":s:u:U:p:P:");
+        // printf("optind: %i, arg: %s\n", optind, argv[optind]);
         switch (c) {
             case 's':
                 port = atoi(optarg);
@@ -56,72 +60,59 @@ int main(int argc, char *argv[]) {
                 printf("Unknown option -%c", optopt);
                 exit(1);
                 break;
+            case -1:
+                //either destination or service
+
+                if (!strncmp(argv[optind], "ssh", 4)) {
+                    service = "ssh";
+                } else {
+                    destination = argv[optind];
+                }
+                optind++;
+                break;
+
         }
     }
 
     /*===Services===*/
 
     //SSH
-    if (!strcmp(argv[8], "ssh")) {
-        char *destination = argv[7];
-        // char *username;
-        // char *password;
-
-        //Configure the options before making an SSH connection
-        fprintf(stdout, "%d",port);
+    if (!strcmp(service, "ssh")) {
         ssh_session my_ssh = ssh_new();
-        ssh_options_set(my_ssh, SSH_OPTIONS_HOST, destination);
-        ssh_options_set(my_ssh, SSH_OPTIONS_PORT, &port);
-
-
-    // if (!strcmp(argv[4], "-u")) {
-    //     *username = argv[5]
-    // } else if (!strcmp(argv[4], "-U")) {
-    //     //Open file, put usernames into a char array
-    //     FILE *fptr;
-
-    // // Open a file in read mode
-    //     fptr = fopen(argv[5], "r");
-    //     username = 
-    // }
-        ssh_options_set(my_ssh, SSH_OPTIONS_USER, usr);
-        
-        //Connect to SSH server
-        int rc = ssh_connect(my_ssh);
-        if (rc != SSH_OK) {
-            fprintf(stderr, "Error connecting to host: %s\n",
-                ssh_get_error(my_ssh));
-            ssh_free(my_ssh);
-            exit(-1);
-        }
-
-        //Verify the server's identity (not sure if this is necessary)
-        // For the source code of verify_knownhost(), check previous example
-        // if (verify_knownhost(my_ssh) < 0) {
-        //     ssh_disconnect(my_ssh);
-        //     ssh_free(my_ssh);
-        //     exit(-1);
-        // }
-
-        //Spray passwords
-       
-        rc = ssh_userauth_password(my_ssh, NULL, pass);
-        if (rc != SSH_AUTH_SUCCESS) {
-            fprintf(stderr, "Error authenticating with password: %s\n",
-                ssh_get_error(my_ssh));
+        int attemptStatus = sshAttempt(destination, usr, pass, my_ssh);
+        if(attemptStatus == SSH_AUTH_SUCCESS) {
+            printf("Authentication successful. || Destination:%s || Username:%s || Password:%s\n", destination, usr, pass);
             ssh_disconnect(my_ssh);
-            ssh_free(my_ssh);
-            exit(-1);
+        } else if(attemptStatus == SSH_AUTH_DENIED) {
+            printf("Authentication failure. Password:%s is not valid for user: %s\n", pass, usr);
+            ssh_disconnect(my_ssh);
         } else {
-            printf("Authentication successful. Username:%s, Password:%s\n", usr, pass);
+            fprintf(stderr, "Error authenticating with password: %s\n",
+            ssh_get_error(my_ssh));
             ssh_disconnect(my_ssh);
-            ssh_free(my_ssh);
         }
-
-
-        
+        ssh_free(my_ssh);   
     }
     
     return 0;
 }
 
+int sshAttempt(char* destination, char* username, char* password, ssh_session my_ssh) {
+    //Configure the options before making an SSH connection
+    ssh_options_set(my_ssh, SSH_OPTIONS_HOST, destination);
+    ssh_options_set(my_ssh, SSH_OPTIONS_PORT, &port);
+    ssh_options_set(my_ssh, SSH_OPTIONS_USER, usr);
+    
+    //Connect to SSH server
+    int rc = ssh_connect(my_ssh);
+    if (rc != SSH_OK) {
+        fprintf(stderr, "Error connecting to host: %s\n",
+            ssh_get_error(my_ssh));
+        ssh_free(my_ssh);
+        exit(-1);
+    }
+
+    //Spray passwords
+    return ssh_userauth_password(my_ssh, NULL, pass);
+    
+}
